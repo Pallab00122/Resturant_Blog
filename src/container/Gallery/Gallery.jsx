b5c150235852baf2e43aeb2c2defd9c86afbfa7c
Pallab Hazra <hazrap404@gmail.com>
2023-07-22 23:18:16 +0530

    "baler"

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
 
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
 
-const galleryimages=[images.gallery01, images.gallery02, images.gallery03, images.gallery04];
+const galleryimages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
 const Gallery = () => {
-  const scrollRef=React.useRef(null);
+  const scrollRef = React.useRef(null);
 
   // const scroll=(direction)=>{
   //   const{current}=scrollRef;
@@ -18,18 +18,18 @@ const Gallery = () => {
   //   }
   // };
 
-  return(
+  return (
     <div className="app__gallery flex__center">
       <div className="app__gallery-content">
-        <SubHeading title="Instagram"/>
+        <SubHeading title="Instagram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
-        <p className="p__opensans" style={{color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
         <button type="button" className="custom__button">View More</button>
       </div>
-      
+
       <div className="app__gallery-images">
         <div className="app__gallery-images_container" ref={scrollRef}>
-          {galleryimages.map((image,index)=>(
+          {galleryimages.map((image, index) => (
             <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
               <img src={image} alt="gallery_image" />
               <BsInstagram className="gallery__image-icon" />
